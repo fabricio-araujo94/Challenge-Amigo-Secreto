4b27ce03e224d1f054b7b1708530534a86a89895
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2025-08-27 16:17:21 -0300

    feat: listarAmigos

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 let amigos = [];
 
-
 function adicionarAmigo() {
     let nome = document.querySelector("#amigo").value;
 
@@ -13,4 +12,15 @@ function adicionarAmigo() {
 
     amigos.push(nome);
     document.querySelector("#amigo").value = "";
+    listarAmigos();
+}
+
+function listarAmigos() {
+    let lista = document.querySelector("#listaAmigos");
+
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
 }
