99cf952a75876fb42f278ecd8bc23e2b2e12a497
Fabricio <fabricio.araujo61@aluno.ifce.edu.br>
2025-08-27 16:01:53 -0300

    feat: adicionarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+
+function adicionarAmigo() {
+    let nome = document.querySelector("#amigo").value;
+
+    if (nome.isEmpty) {
+        alert("Por favor, insira um nome.");
+        return;
+    };
+
+    amigos.push(nome);
+    document.querySelector("#amigo").value = "";
+}
